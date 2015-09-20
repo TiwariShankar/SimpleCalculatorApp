891cf2a91eafc7616cf69d4531a13a11a94098a1
Shanky <shanky@shankys-mac.local>
2015-09-20 06:02:13 +0530

    Layout editing done

--- SimpleCalculatorApp/ViewController.swift
+++ SimpleCalculatorApp/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userinmiddleofwriting :Bool = false
+    var arrayStack = Array<Double>();
     
-    @IBAction func keypressed(sender: UIButton) {
-    
+    @IBAction func digitPressed(sender: UIButton) {
         let digit = sender.currentTitle!
-       
+        
         if(userinmiddleofwriting){
             display.text = display.text! + digit
         }else{
@@ -27,6 +27,57 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func enterPressed() {
+        userinmiddleofwriting = false
+        arrayStack.append(displayVal)
+       print("\(arrayStack)")
+    }
     
+    
+    @IBAction func operatorPressed(sender: UIButton) {
+        let caseoperator = sender.currentTitle!
+        if userinmiddleofwriting{
+            enterPressed()
+        }
+        
+        switch caseoperator {
+            case "+":
+                performOperation {$0 + $1}
+                break
+            
+            case "-":
+                performOperation {$0 - $1}
+                break
+            
+            case "/":
+                performOperation {$0 / $1}
+                break
+            
+            case "*":
+                performOperation {$0 * $1}
+                break
+            
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double){
+        if arrayStack.count >= 2 {
+            displayVal = operation(arrayStack.removeLast(), arrayStack.removeLast())
+            enterPressed()
+        }
+        
+    }
+    
+    var displayVal : Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userinmiddleofwriting = false
+        }
+    }
 }
+
 
